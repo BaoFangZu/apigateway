723f490c20b28e24da97c29f18d1e4b0325ce366
root <root@dev.novalocal>
2016-11-17 11:51:45 +0000

    config changed

--- src/main/java/com/huawei/it/hicloud/iam/gateway/controller/APIGatewayController.java
+++ src/main/java/com/huawei/it/hicloud/iam/gateway/controller/APIGatewayController.java
@@ -1,31 +1,11 @@
 package com.huawei.it.hicloud.iam.gateway.controller;
 
-import java.net.URLEncoder;
-import java.util.HashMap;
-import java.util.Map;
-
-import javax.servlet.http.HttpServletRequest;
-
 import org.apache.log4j.Logger;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.ApplicationContextException;
-import org.springframework.http.HttpEntity;
-import org.springframework.http.HttpHeaders;
-import org.springframework.http.HttpMethod;
-import org.springframework.http.ResponseEntity;
-import org.springframework.security.core.context.SecurityContextHolder;
-import org.springframework.stereotype.Controller;
-import org.springframework.util.Base64Utils;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.client.RestTemplate;
-import org.springframework.web.util.UrlPathHelper;
+import org.springframework.web.bind.annotation.RestController;
 
 
-@Controller
+@RestController
 @RequestMapping("/api")
 public class APIGatewayController{
 
