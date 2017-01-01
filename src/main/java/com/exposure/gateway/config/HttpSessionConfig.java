9a1306158f6d4620310a6b4ec03fcdfc105a5234
root <root@dev.novalocal>
2017-01-01 05:33:27 +0000

    change

--- src/main/java/com/exposure/gateway/config/HttpSessionConfig.java
+++ src/main/java/com/exposure/gateway/config/HttpSessionConfig.java
@@ -1,5 +1,6 @@
 package com.exposure.gateway.config;
 
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.connection.jedis.JedisConnectionFactory;
@@ -20,6 +21,7 @@ public class HttpSessionConfig {
         }*/
 
         @Bean
+        @ConditionalOnProperty(value="token.header.enabled", matchIfMissing=true)
         public HttpSessionStrategy httpSessionStrategy() {
                 return new HeaderHttpSessionStrategy(); 
         }
